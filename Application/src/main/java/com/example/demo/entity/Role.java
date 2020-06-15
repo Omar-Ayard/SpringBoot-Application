906df04e9bba09960c16732791c793ddd15331fe
Omar-Ayard <omar100395@hotmail.com>
2020-06-14 23:00:44 -0500

    Role and User entities Created. Close #2

---
+++ Application/src/main/java/com/example/demo/entity/Role.java
@@ -0,0 +1,95 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Role implements Serializable
+{
+
+	/**
+	 * The Serial version for the class
+	 */
+	private static final long serialVersionUID = -2582289195430057769L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+	@GenericGenerator(name="native",strategy = "native")
+	private long id;
+	
+	@Column
+	private String roleName;
+	
+	@Column
+	private String roleDescription;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getRoleDescription() {
+		return roleDescription;
+	}
+
+	public void setRoleDescription(String roleDescription) {
+		this.roleDescription = roleDescription;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", roleName=" + roleName + ", roleDescription=" + roleDescription + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((roleDescription == null) ? 0 : roleDescription.hashCode());
+		result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id != other.id)
+			return false;
+		if (roleDescription == null) {
+			if (other.roleDescription != null)
+				return false;
+		} else if (!roleDescription.equals(other.roleDescription))
+			return false;
+		if (roleName == null) {
+			if (other.roleName != null)
+				return false;
+		} else if (!roleName.equals(other.roleName))
+			return false;
+		return true;
+	}
+		
+}
